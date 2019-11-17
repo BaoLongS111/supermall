e0431d6d898a0c35c413cc3dc799102ddf2ef87d
BaoLongS111 <51baolongstudio@gmail.com>
2019-11-17 18:08:17 +0800

    test

--- src/common/utils.js
+++ src/common/utils.js
@@ -10,10 +10,22 @@ export function debounce(func, delay) {
     }
   }
 
+  //正则表达式 字符串匹配
   export function formatDate(date, fmt) {
+
+    //1.获取年份
+    //y+  -> 1个或者多个y
+    /**
+     * test
+     */
+    //yyyy-MM-dd
+    //2019-11-27
     if (/(y+)/.test(fmt)) {
       fmt = fmt.replace(RegExp.$1, (date.getFullYear() + '').substr(4 - RegExp.$1.length));
     }
+    //RegEXP.$1 代表全局匹配到的结果   2019
+
+
     let o = {
       'M+': date.getMonth() + 1,
       'd+': date.getDate(),
@@ -30,6 +42,7 @@ export function debounce(func, delay) {
     return fmt;
   };
 
+  //补零算法 例如 4小时4分4秒
   function padLeftZero (str) {
     return ('00' + str).substr(str.length);
   };
